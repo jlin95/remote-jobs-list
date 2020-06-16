0d637343762f939405da8150f817e74a76a9d775
Sam <leeway95@gmail.com>
2020-06-17 00:42:32 +0800

    Added skeleton styling

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,47 @@
   
+<style>
+.class {
+  /* font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif; */
+  font-size: 32px;
+  color: black;
+}
+a:link {
+  text-decoration: none;
+  color: grey;
+}
+a:hover {
+  text-decoration: underline;
+  color: black;
+}
+a:visited {
+  color: grey;
+}
+</style>
+
 <template>
   <div id="app">
     <h1>Remote OK Jobs Board</h1>
-    <div
-      v-for="job in jobs"
-      class="job"
-      v-bind:key="job"
-    >
-    <a v-bind:href="job.url" v-bind:key="job.url">{{ job.position }}</a>
-
+    <div v-for="job in jobs" class="job" v-bind:key="job">
+      <a v-bind:href="job.url">{{ job.position }}</a>
     </div>
-
   </div>
 </template>
 
 <script>
 import axios from "axios";
 export default {
-  name: 'app',
-  data () {
+  name: "app",
+  data() {
     return {
       jobs: null
-    }
+    };
   },
   mounted() {
-    axios
-      .get('https://remoteok.io/api')
-      .then(response => {
-        response.data.shift()
-        this.jobs = response.data
-        return this.jobs
-      }
-  )
-}}
-
+    axios.get("https://remoteok.io/api").then(response => {
+      response.data.shift();
+      this.jobs = response.data;
+      return this.jobs;
+    });
+  }
+};
 </script>
