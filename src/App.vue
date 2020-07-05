bf5edda0de068a33ac7c8fff60147cce20eef77f
Sam Lee <leeway95@gmail.com>
2020-07-06 00:37:13 +0800

    more styling

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 }
 a:link {
   text-decoration: none;
-  color: grey;
+  color: black;
 }
 a:hover {
   text-decoration: underline;
@@ -16,13 +16,43 @@ a:hover {
 a:visited {
   color: grey;
 }
+.job-item {
+  border-radius: 4px;
+  width: 300px;
+  padding: 8px 10px;
+  margin: 0 auto 16px;
+  -webkit-box-shadow: 10px 10px 16px 0px rgba(99, 131, 131, 0.16);
+  -moz-box-shadow: 10px 10px 16px 0px rgba(99, 131, 131, 0.16);
+  box-shadow: 10px 10px 16px 0px rgba(99, 131, 131, 0.16);
+}
+
+.job-item > div > img {
+  border-radius: 10px;
+  width: 100px;
+  height: 100%;
+  object-fit: cover;
+}
+
+.company {
+  font-size: 15px;
+  color: #638383;
+  font-weight: bold;
+  margin: 0;
+}
+
+.position {
+  color: black;
+}
 </style>
 
 <template>
   <div id="app">
     <h1>Remote OK Jobs Board</h1>
-    <div v-for="job in jobs" class="job" v-bind:key="job">
-      <a v-bind:href="job.url">{{ job.position }}</a>
+    <div v-for="job in jobs" class="job" v-bind:key="job.id">
+      <div key="job.id" class="job-item">
+        <p class="company">{{ job.company }}</p>
+        <a class="position" v-bind:href="job.url">{{ job.position }}</a>
+      </div>
     </div>
   </div>
 </template>
